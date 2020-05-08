1fd57e33633bfd2e9819568dbd262dc6e365098c
anju <anju.p@momscode.in>
2020-05-08 02:17:51 +0000

    Change UOM field as Link field

--- electrical_contracting/electrical_contracting/doctype/activity_item/activity_item.js
+++ electrical_contracting/electrical_contracting/doctype/activity_item/activity_item.js
@@ -1,8 +1,3 @@
 // Copyright (c) 2020, momscode and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Activity Item', {
-	// refresh: function(frm) {
-
-	// }
-});
